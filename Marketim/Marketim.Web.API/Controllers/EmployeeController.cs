5d3ad8e2a2f1cfbf97c37c8268bb5a3416d10449
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2022-02-22 22:01:27 +0300

    init

---
+++ Marketim/Marketim.Web.API/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using Marketim.Business.Abstracts;
+using Marketim.Entity.Concretes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Marketim.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employeeService.GetAll();
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _employeeService.Get(x=>x.Id == id);
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] Employee employee)
+        {
+            _employeeService.Insert(employee);
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee employee)
+        {
+            employee.Id = id;
+            _employeeService.Update(employee);
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _employeeService.Delete(new Employee { Id = id });
+        }
+    }
+}
---
+++ Marketim/Marketim.Web.API/Controllers/ManagerController.cs
@@ -0,0 +1,63 @@
+﻿using Marketim.Business.Abstracts;
+using Marketim.Entity.Concretes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Marketim.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+
+        private readonly IManagerService _managerService;
+        public ManagerController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+
+        // GET: api/<ManagerController>
+        [HttpGet]
+        public IEnumerable<Manager> Get()
+        {
+            return _managerService.GetAll();
+        }
+
+        // GET api/<ManagerController>/5
+        [HttpGet("{id}")]
+        public Manager Get(int id)
+        {
+            return _managerService.Get(x => x.Id == id);
+        }
+
+        // POST api/<ManagerController>
+        [HttpPost]
+        public void Post([FromBody] Manager manager)
+        {
+
+            _managerService.Insert(manager);
+
+        }
+
+        // PUT api/<ManagerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Manager manager)
+        {
+            manager.Id = id;
+            _managerService.Update(manager);
+        }
+
+        // DELETE api/<ManagerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _managerService.Delete(new Manager { Id = id });
+        }
+    }
+}
